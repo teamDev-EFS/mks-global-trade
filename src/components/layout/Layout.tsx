350be70edf15b8f4ce4d4910b5f563a6fd9dc918
teamDev-EFS <team-dev@epicforgesoftware.com>
2026-03-27 00:15:34 +0530

    Auto-sync from PromptFloe

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import Sidebar from './Sidebar';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <Navbar />
+      <Sidebar />
       <main className="flex-grow">
         {children}
       </main>
